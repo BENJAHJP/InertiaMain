2e8beeb604532040c926492904fa9b1821830e78
BENJAHJP <benjahj@gmail.com>
2021-11-25 16:13:53 +0300

    commit

--- app/src/main/java/com/example/inertia/Dashboard/ExploreFragment.kt
+++ app/src/main/java/com/example/inertia/Dashboard/ExploreFragment.kt
@@ -1,17 +1,17 @@
-package com.example.inertia
+package com.example.inertia.Dashboard
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.inertia.R
 
-class DashboardFragment : Fragment() {
+class ExploreFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 }
