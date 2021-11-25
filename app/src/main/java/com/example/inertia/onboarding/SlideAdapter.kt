acd1056c76c477d12525dc33596d48fac31b7bb0
BENJAHJP <benjahjp@email.com>
2021-11-25 11:39:11 +0300

    commit

---
+++ app/src/main/java/com/example/inertia/onboarding/SlideAdapter.kt
@@ -0,0 +1,14 @@
+package com.example.inertia.onboarding
+
+import android.view.View
+import androidx.viewpager.widget.PagerAdapter
+
+class SlideAdapter : PagerAdapter() {
+    override fun getCount(): Int {
+
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+
+    }
+}
