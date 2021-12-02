7125099cf55554a6d72bf7ab945a27bd871dd039
BENJAHJP <benjahjp@email.com>
2021-12-02 10:10:59 +0300

    commit

--- app/src/main/java/com/example/inertia/Dashboard/UserSettings.kt
+++ app/src/main/java/com/example/inertia/Dashboard/UserSettings.kt
@@ -21,11 +21,11 @@ class UserSettings : Fragment() {
 
         val id: Int = databaseConfig.usersDao()!!.getLastId()
         var userText = view.findViewById<TextView>(R.id.userNameSettings)
-        val helloText: String = "Hello"
+        val helloText = "Hello"
 
         if (databaseConfig.usersDao()?.getData(id) == true){
             val usersEntity: UsersEntity =  databaseConfig.usersDao()!!.getName(id)
-            userText.text = "$helloText usersEntity.name"
+            userText.text = "$helloText" +" "+usersEntity.name
         }
         return view
     }
